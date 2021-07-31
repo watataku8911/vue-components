b462e2676ab0021160c9d03cd106570fc5808d56
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-07-31 17:54:04 +0900

    Radio-button component

---
+++ components/RadioButton.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="contents">
+    <!--テキストフィールド -->
+    <TextInput
+      v-model="text"
+      type="text"
+      placeholder="テキストボックス"
+      :value="text"
+    />
+
+    <!--パスワード-->
+    <TextInput
+      v-model="pass"
+      type="password"
+      placeholder="パスワード"
+      :value="pass"
+    />
+
+    <!-- テキストエリア -->
+    <TextArea
+      v-model="textarea"
+      placeholder="テキストエリア"
+      cols="76"
+      rows="5"
+      :value="textarea"
+    />
+
+    <!-- ラジオボタン -->
+    <RadioButton v-model="checkName" :options="options" />
+    <span>選択オプション: {{ checkName }}</span>
+    <div class="module--spacing--small"></div>
+
+    <!-- チェックボックス -->
+    <CheckBox v-model="checked" :checked="checked" />
+    <label>同意</label><br />
+
+    <!-- ボタン -->
+    <Button :disabled="!checked" msg="ボタン" @push="click" />
+  </div>
+</template>
+
+<script>
+import TextInput from "./components/TextInput.vue";
+import TextArea from "./components/TextArea.vue";
+import CheckBox from "./components/CheckBox.vue";
+import RadioButton from "./components/RadioButton.vue";
+import Button from "./components/Button.vue";
+
+export default {
+  components: {
+    TextInput,
+    TextArea,
+    CheckBox,
+    RadioButton,
+    Button,
+  },
+  data() {
+    return {
+      text: "",
+      pass: "",
+      textarea: "",
+      checkName: "選択してね",
+      options: [
+        { label: "hoge", value: "1", checked: false },
+        { label: "bow", value: "2", checked: false },
+        { label: "fuga", value: "3", checked: false },
+      ],
+      checked: false,
+    };
+  },
+  methods: {
+    click() {
+      console.log("テキストボックスの入力内容：", this.text);
+      console.log("パスワードの入力内容：", this.pass);
+      console.log("テキストエリアの入力内容：", this.textarea);
+    },
+  },
+};
+</script>
+
+<style>
+.contents {
+  width: 80%;
+  margin: 0% auto;
+  text-align: center;
+}
+
+.module--spacing--small {
+  height: 20px;
+}
+</style>
