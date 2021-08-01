11b9ad2ba3ca1343314b943039e640f7573dbd24
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-08-01 12:39:16 +0900

    create SelectBox  Conponent

---
+++ components/SelectBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <select name="select-box" @input="updateValue">
+    <option value="0">選択してください</option>
+    <option
+      v-for="(option, index) in options"
+      :key="index"
+      :value="option.value"
+      >{{ option.label }}</option
+    >
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    options: { type: Array, required: true },
+  },
+  methods: {
+    updateValue(e) {
+      this.$emit("input", e.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+select {
+  text-align: center;
+  display: block;
+}
+</style>
